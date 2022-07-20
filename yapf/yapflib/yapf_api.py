82b1d11ffc381750bfd2fc704dfaf9006e4e560f
naelsondouglas <naelson17@gmail.com>
2022-07-20 00:17:49 -0300

    whatever

--- yapf/yapflib/yapf_api.py
+++ yapf/yapflib/yapf_api.py
@@ -104,7 +104,9 @@ def FormatFile(filename,
       file_resources.WriteReformattedCode(filename, reformatted_source,
                                           encoding, in_place)
     return None, encoding, changed
-
+  tree = ast.parse(reformatted_source)
+  result = Visitor().visit(tree)
+  reformatted_source = ast.unparse(result)
   return reformatted_source, encoding, changed
 
 
