c6ccbfce52a1be3b7bbcda2c459364da2f033964
naelsondouglas <naelson17@gmail.com>
2022-07-19 23:45:52 -0300

    expanded an import

--- yapf/yapflib/yapf_api.py
+++ yapf/yapflib/yapf_api.py
@@ -49,8 +49,7 @@ from yapf.yapflib import identify_container
 from yapf.yapflib import py3compat
 from yapf.yapflib import reformatter
 from yapf.yapflib import style
-
-
+from yapf.yapflib.warning_remover.engine import Visitor
 def FormatFile(filename,
                style_config=None,
                lines=None,
