2a335a598db9b9d3a4a093fb13393d9d88103255
naelsondouglas <naelson17@gmail.com>
2022-07-19 23:44:58 -0300

    removed a bug

--- yapf/yapflib/yapf_api.py
+++ yapf/yapflib/yapf_api.py
@@ -49,7 +49,6 @@ from yapf.yapflib import identify_container
 from yapf.yapflib import py3compat
 from yapf.yapflib import reformatter
 from yapf.yapflib import style
-from yapf.yapflib import LintWarningRemover
 
 
 def FormatFile(filename,
